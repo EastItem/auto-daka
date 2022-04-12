59f5dab3c7010337d417d3e8a2184534a5999466
EastItem <88192911+EastItem@users.noreply.github.com>
2022-04-12 16:55:03 +0800

    fix bug

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ def main_handler(event, context):
 	res=yiban.daka(flag='1',accountid='1',password='1',address='广东省广州市天河区迎福路靠近广东金融学院(广州校区)') 
 	#分别填写昵称、账号、密码（易班）、打卡地址等信息
 	print(res)
-	notify(sendKey='',notifyType=1,code=res.code)
+	notify(sendKey='',notifyType=1,code=res["code"])
 
 def notify(sendKey='',notifyType=0,code=0):
 	"""  
